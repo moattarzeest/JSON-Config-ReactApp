22ba1ac5ec6ea9144b73546403c75bb50680f195
Moattar Zeest Rana <90420556+moattarzeest@users.noreply.github.com>
2023-09-07 09:21:01 +0500

    Update ButtonComponent.js

--- src/components/ButtonComponent.js
+++ src/components/ButtonComponent.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const ButtonComponent = ({ label, action }) => {
-  return <button onClick={() => eval(action)} className="buttonComponent">{label}</button>;
+  return <button onClick={() => alert(action)} className="buttonComponent">{label}</button>;
 };
 
 export default ButtonComponent;
