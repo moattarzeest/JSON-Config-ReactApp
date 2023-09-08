fc93db3a7d5218b467842e561e0528386fb6459b
Moattar Zeest Rana <90420556+moattarzeest@users.noreply.github.com>
2023-09-08 10:32:34 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,39 +1,14 @@
-import React from 'react';
-import TextComponent from './components/TextComponent';
-import ImageComponent from './components/ImageComponent';
-import ButtonComponent from './components/ButtonComponent';
+import React from "react";
+import Renderer from "./RenderFunction";
 
-
-const jsonConfig = 
-[
-  { "type": "text",
-    "content": "Hey fella! Meet my pet cat Ziry!" 
-  },
-
-  { "type": "image", 
-    "src": "/cocomo.jpeg", 
-    "alt": "My pet cat Ziry" 
-  },
-  { "type": "button", 
-    "label": "Mark as done", 
-    "action": "Done!!" 
-  }
+const jsonConfig = [
+  { component: "text", content: "Hey fellas! Meet my pet cat Ziry!" },
+  { component: "image", src: "/cocomo.jpeg", alt: "My pet cat Ziry" },
+  { component: "button", label: "Cute cat", action: "Cat checked!!" }
 ];
 
-
 export default function App() {
-  const renderedComponents = jsonConfig.map((config, index) => {
-    switch (config.type) {
-      case 'text':
-        return <TextComponent key={index} content={config.content}  />;
-      case 'image':
-        return <ImageComponent key={index} src={config.src} alt={config.alt} className="imageComponent"/>;
-      case 'button':
-        return <ButtonComponent key={index} label={config.label} action={config.action} className="buttonComponent" />;
-      default:
-        return null; 
-    }
-  });
+  const renderedComponents = jsonConfig.map((config) => Renderer(config));
 
   return <div>{renderedComponents}</div>;
 }
