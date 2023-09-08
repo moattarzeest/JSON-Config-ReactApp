810922ef85c33580388024ccc44a929337cc5c37
Moattar Zeest Rana <90420556+moattarzeest@users.noreply.github.com>
2023-09-08 11:14:53 +0500

    Update ButtonComponent.js

--- src/components/ButtonComponent.js
+++ src/components/ButtonComponent.js
@@ -1,5 +1,7 @@
 import React from "react";
 
-export default (props) => (
+const buttonComponents =  (props) => (
    <button className="button" onClick={()=>alert(props.config.action)}>{props.config.label}</button>
 );
+
+export default buttonComponents;
