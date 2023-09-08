2abec4e1c0a02a0b3454ed0fd0120a3d6da4e233
Moattar Zeest Rana <90420556+moattarzeest@users.noreply.github.com>
2023-09-08 10:37:19 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from "react";
 import Renderer from "./RenderFunction";
-
 const jsonConfig = [
   { component: "text", content: "Hey fellas! Meet my pet cat Ziry!" },
   { component: "image", src: "/cocomo.jpeg", alt: "My pet cat Ziry" },
   { component: "button", label: "Cute cat", action: "Cat checked!!" }
 ];
-
 export default function App() {
   const renderedComponents = jsonConfig.map((config) => Renderer(config));
-
   return <div>{renderedComponents}</div>;
 }
